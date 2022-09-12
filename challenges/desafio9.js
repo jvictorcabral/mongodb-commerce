73e2c43484d829f9eddf6a97e46a5f8504001877
Joao <joaovictorcgjvcg@gmail.com>
2022-09-12 12:05:12 -0400

    Entregando o requisito 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,5 +1,8 @@
-db.produtos.find({
-valoresNutricionais: {
-  $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } },
-} },
-{ _id: false, nome: true });
+db.produtos
+  .find(
+    {
+      valoresNutricionais: 
+        { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } },
+    } },
+    { _id: false, nome: true },
+  );
